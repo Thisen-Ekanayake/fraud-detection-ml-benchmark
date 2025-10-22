142688831ff5375ef31d7bb8724acdda07cef236
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-10-22 12:55:47 +0530

    data loading and preprocessing

---
+++ random_forest_training.py
@@ -0,0 +1,25 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    confusion_matrix, classification_report,
+    roc_auc_score, precision_recall_curve,
+    auc, roc_curve
+)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import os
+
+df = pd.read_csv('creditcard.csv')
+
+X = df.drop('Class', axis=1)
+y = df['Class']
+
+scaler = StandardScaler()
+X[['Time', 'Amount']] = scaler.fit_transform(X[['Time', 'Amount']])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
