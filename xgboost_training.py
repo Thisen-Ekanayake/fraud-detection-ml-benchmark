2477bc957da5b50a3280234db52fb1feb8fb32b0
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-10-27 12:04:45 +0530

    fixed callback error

--- xgboost_training.py
+++ xgboost_training.py
@@ -53,7 +53,7 @@ def objective(trial):
         'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
         'scale_pos_weight': scale_pos_weight,
         'eval_metric': 'auc',
-        'use_label_encoder': False,
+        'early_stopping_rounds': 30,
         'random_state': 42,
         'n_jobs': -1,
     }
@@ -63,13 +63,13 @@ def objective(trial):
     model.fit(
         X_train, y_train,
         eval_set=[(X_test, y_test)],
-        early_stopping_rounds=30,
         verbose=False
     )
 
     y_pred_proba = model.predict_proba(X_test)[:, 1]
     roc_auc = roc_auc_score(y_test, y_pred_proba)
     return roc_auc
+
 
 
 # ========================================
@@ -93,7 +93,6 @@ best_params = study.best_params
 best_params.update({
     'scale_pos_weight': scale_pos_weight,
     'eval_metric': 'auc',
-    'use_label_encoder': False,
     'random_state': 42,
     'n_jobs': -1
 })
